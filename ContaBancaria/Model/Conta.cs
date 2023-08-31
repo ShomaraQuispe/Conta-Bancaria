aa87e49c201a878fc672bec11fb0ee5cb7f33e0e
ShomaraQuispe <shomaraclaudia@gmail.com>
2023-08-31 12:08:34 -0300

    Model Conta

---
+++ ContaBancaria/Model/Conta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria.Model
+{
+    internal class Conta
+    {
+    }
+}
